6d5e63ab44dbe6d4b2ead3b37a5c7a92175756c0
navarrx <sa.navarro@alumno.um.edu.ar>
2025-05-16 01:08:36 -0300

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -46,4 +46,5 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True) 
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True) 
