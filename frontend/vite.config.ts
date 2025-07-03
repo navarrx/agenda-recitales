b094ffec77fba22707b5c7bf195b1b27e7eebdbd
navarrx <sa.navarro@alumno.um.edu.ar>
2025-07-03 13:05:52 -0300

    chore(frontend): actualizar dependencias y configuración general

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
       'healthcheck.railway.app',
       'localhost',
       '127.0.0.1',
-      'agenda-recitales-production.up.railway.app',
+      // 'agenda-recitales-production.up.railway.app',
       '*.up.railway.app'
     ]
   }
